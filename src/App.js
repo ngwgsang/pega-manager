d230526dc5953d7d4e05aeccdd24f875b174d825
Sang Cá Mập <55171560+sangcamap@users.noreply.github.com>
2022-04-09 23:23:11 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://avatars.githubusercontent.com/u/55171560?v=4"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Come here !
         </p>
         <a
           className="App-link"
